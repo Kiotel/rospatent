4a976dafed310757001e8f04e11582dbadbf81fd
kiotel <valera.lihenko@gmail.com>
2024-08-10 15:54:13 +0300

    Улучшен визуал рассматривания патента 👌

--- app/src/main/java/com/example/rospatent/foundScreen.kt
+++ app/src/main/java/com/example/rospatent/foundScreen.kt
@@ -1,6 +1,7 @@
 package com.example.rospatent
 
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -30,8 +31,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,16 +43,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.fastJoinToString
 import androidx.compose.ui.window.Dialog
 import com.example.rospatent.classes.Patent
-
 
 @Composable
 fun FoundScreen(
@@ -95,7 +101,9 @@ fun FoundScreen(
       }
 
       true -> {
-        PatentResult(patent = chosenPatent, onBack = { isChosenPatent = false })
+        PatentResult(modifier = Modifier.padding(15.dp),
+          patent = chosenPatent,
+          onBack = { isChosenPatent = false })
       }
     }
   }
@@ -209,7 +217,7 @@ fun PatentCard(modifier: Modifier = Modifier, patent: Patent) {
       Icon(painter = painterResource(id = R.drawable.patent_list_icon), contentDescription = null)
       Column {
         Text(
-          text = patent.snippet.title, overflow = TextOverflow.Ellipsis, maxLines = 1
+          text = patent.biblio.ru.title, overflow = TextOverflow.Ellipsis, maxLines = 1
         )
         Row(horizontalArrangement = Arrangement.SpaceBetween) {
           Text(text = patent.snippet.inventor, fontSize = 3.em)
@@ -319,6 +327,7 @@ fun PatentsList(
         PatentCard(
           modifier = Modifier
             .height(50.dp)
+            .fillMaxWidth()
             .clickable(onClick = { onPatentChose(patent) }),
           patent = patent
         )
@@ -334,53 +343,138 @@ fun PatentsList(
 }
 
 @Composable
-fun PatentResult(modifier: Modifier = Modifier, patent: Patent, onBack: () -> Unit) {
+fun PatentResult(
+  modifier: Modifier = Modifier,
+  patent: Patent = Patent(),
+  onBack: () -> Unit = {},
+) {
+  var isShowMore by rememberSaveable { mutableStateOf(false) }
   Icon(modifier = Modifier
     .clickable { onBack() }
     .size(40.dp),
     imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowLeft,
     contentDescription = "back",
     tint = Color.Black)
-  Column(modifier = modifier.verticalScroll(rememberScrollState())) { // TODO: СДЕЛАТЬ СТОЛБИКИ НОРМАЛЬНО
+  Column(modifier = modifier.verticalScroll(rememberScrollState())) {
     Text(
-      "Авторы: ${patent.snippet.inventor}"
+      patent.biblio.ru.title,
+      color = Color.Black,
+      fontWeight = FontWeight.Black
     )
-    Column(modifier = Modifier.fillMaxWidth()) {
-      Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-        Column {
-          Text(text = "Номер заявки:")
-          Text(text = patent.common.application.number)
+    Text(
+      "Авторы: ${patent.biblio.ru.inventor.joinToString { inventor -> inventor.name }}",
+      fontSize = 12.sp,
+      color = Color.DarkGray,
+      fontWeight = FontWeight.Light
+    )
+    Row(
+      modifier = Modifier.fillMaxWidth(),
+    ) {
+      Column(modifier = Modifier.weight(weight = 1.0f)) {
+        Text(
+          text = "Номер заявки:",
+          color = Color.Black,
+          fontWeight = FontWeight.Black,
+          fontSize = 12.sp
+        )
+        Text(
+          text = patent.common.application.number, color = Color.Black,
+          fontSize = 12.sp
+        )
+        Text(
+          text = "Дата подачи заявки:", color = Color.Black, fontWeight = FontWeight.Black,
+          fontSize = 12.sp
+        )
+        Text(
+          text = patent.common.application.filingDate, color = Color.Black,
+          fontSize = 12.sp
+        )
+
+        AnimatedVisibility(isShowMore) {
+          Column {
+
+            Text(
+              text = "Приоритетные данные:", color = Color.Black, fontWeight = FontWeight.Black,
+              fontSize = 12.sp
+            )
+            Text(
+              text = patent.common.application.number, color = Color.Black,
+              fontSize = 12.sp
+            )
+            Text(
+              text = patent.snippet.lang, color = Color.Black,
+              fontSize = 12.sp
+            )
+            Text(
+              text = patent.id, color = Color.Black,
+              fontSize = 12.sp
+            )
+            Text(
+              text = patent.common.kind, color = Color.Black,
+              fontSize = 12.sp
+            )
+            Text(
+              text = patent.index, color = Color.Black,
+              fontSize = 12.sp
+            )
+          }
         }
-        Column {
-          Text(text = "Опубликовано:")
-          Text(text = patent.common.publicationDate)
+        TextButton(onClick = { isShowMore = !isShowMore }) {
+          Text(
+            text = if (isShowMore) {
+              "Скрыть"
+            } else {
+              "Показать больше"
+            }, color = Color.Black,
+            fontSize = 12.sp
+          )
         }
       }
-      Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-        Column {
-          Text(text = "Дата подачи заявки:")
-          Text(text = patent.common.application.filingDate)
-        }
-        Column {
-          Text(text = "МПК:")
-          Text(text = patent.snippet.classification.ipc)
-        }
-      }
-      Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-        Column {
-          Text(text = "Приоритетные данные:")
-          Text(text = "ЧТО_ТО")
-        }
-        Column {
-          Text(text = "Патентообладатели:")
-          Text(text = patent.snippet.patentee)
+      Column(
+        modifier = Modifier
+          .weight(weight = 1.0f)
+          .padding(start = 23.dp)
+      ) {
+        Text(
+          text = "Опубликовано:", color = Color.Black, fontWeight = FontWeight.Black,
+          fontSize = 12.sp
+        )
+        Text(
+          text = patent.common.publicationDate, color = Color.Black,
+          fontSize = 12.sp
+        )
+        Text(
+          text = "МПК:", color = Color.Black, fontWeight = FontWeight.Black,
+          fontSize = 12.sp
+        )
+        Text(
+          text = patent.snippet.classification.ipc, color = Color.Black,
+          fontSize = 12.sp
+        )
+        AnimatedVisibility(isShowMore) {
+          Column {
+
+            Text(
+              text = "Патентообладатели:", color = Color.Black,
+              fontSize = 12.sp, fontWeight = FontWeight.Black
+            )
+            Text(
+              text = patent.biblio.ru.patentee.joinToString { patentee -> patentee.name },
+              color = Color.Black,
+              fontSize = 12.sp
+            )
+          }
         }
       }
     }
     Column {
-      Text(text = "Описание")
       Text(
-        text = patent.snippet.description
+        text = "Описание", color = Color.Black,
+        fontSize = 12.sp
+      )
+      Text(
+        text = patent.snippet.description, color = Color.Black,
+        fontSize = 12.sp
       )
     }
   }
--- app/src/main/java/com/example/rospatent/ui/theme/Theme.kt
+++ app/src/main/java/com/example/rospatent/ui/theme/Theme.kt
@@ -1,20 +1,14 @@
 package com.example.rospatent.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
-private val LightColorScheme = lightColorScheme(
+private val colorScheme = lightColorScheme(
   primary = Blue,
   secondary = BrightBlue,
   tertiary = DarkBlue,
@@ -27,7 +21,7 @@ fun RospatentTheme(
   // Dynamic color is available on Android 12+
   content: @Composable () -> Unit,
 ) {
-  val colorScheme = LightColorScheme
+  val colorScheme = colorScheme
 
   val systemUiController = rememberSystemUiController()
 
